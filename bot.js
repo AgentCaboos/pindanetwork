935df39680d9f98c31fe29e44bc2ff554876b604
AgentCaboos <40305951+AgentCaboos@users.noreply.github.com>
2018-06-21 20:29:51 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '!'
+
+client.on("ready", async () => {
+  console.log('Bot Is Succesvol Opgestart!')
+  client.user.setActivity("TV", {
+    type: "WATCHING"
+  });
+});
+
+client.on('message', message => {
+  let messageArray = message.content.split(" ");
+  let command = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (command.toLowerCase() === `${prefix}ping`) {
+    message.channel.send('pong!')
+  }
+
+  if (command.toLowerCase() === `${prefix}membercount`) {
+    let members = `Er zijn momenteel ${message.guild.memberCount}bezoekers in deze discord server!`;
+    message.channel.send(members)
+  }
+});
+client.on("guildMemberAdd", member => {
+  //als je een role wilt toevoegen als iemand joint  member.addRole(member.guild.roles.find("name","Human"));
+
+  var embed = new Discord.RichEmbed()
+    .setColor(0x13ce26)
+    .setDescription(` ** ${member.user.username} ** Joined!: wave: \nUsercount: ** ${member.guild.members.size}! ** `)
+    .setThumbnail(member.user.avatarURL)
+    .setFooter(member.user.username);
+
+  member.guild.channels.find("id", "459409599389368360").send({
+    embed
+  })
+});
+
+
+client.on("guildMemberRemove", member => {
+  const channel = member.guild.channels.find("id", "459409599389368360")
+
+  var embed = new Discord.RichEmbed()
+    .setColor(0xf48428)
+    .setThumbnail(member.user.avatarURL)
+    .setDescription(` ** ${member.user.username} ** leaved!: wave: \nUsercount: ** ${member.guild.members.size}! ** `)
+    .setFooter(member.user.username);
+  channel.send({
+    embed
+  });
+});
+client.login("NDU5MDMyODI1MDY1MzczNjk2.DgwTTg.nxGeBZm8OE_zeD8Bvi0JGhkPMsc");
